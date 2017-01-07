1ff747dbdfb1d15e40909e316b748d7972ac4a2b
KevinDockx <kevin.dockx@gmail.com>
2017-01-07 11:58:56 +0100

    Expiration and Valdation model options classes added

---
+++ src/Marvin.Cache.Headers/ExpirationModelOptions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marvin.Cache.Headers
+{
+    /// <summary>
+    /// Options that have to do with the expiration model, mainly related to Cache-Control & Expires headers on the response.
+    /// </summary>
+    public class ExpirationModelOptions
+    {
+        /// <summary>
+        /// Maximum age, in seconds, after which a response expires. Has an effect on Expires & on the max-age directive
+        /// of the Cache-Control header.
+        /// 
+        /// Defaults to 600.
+        /// </summary>
+        public int MaxAge { get; set; } = 600;
+
+        /// <summary>
+        /// Maximum age, in seconds, after which a response expires for shared caches.  If included, 
+        /// a shared cache should use this value rather than the max-age value. (s-maxage directive)
+        /// 
+        /// Not set by default.
+        /// </summary>
+        public int? SharedMaxAge { get; set; }
+
+        /// <summary>
+        /// The location where a response can be cached.  Public means it can be cached by both
+        /// public (shared) and private (client) caches.  Private means it can only be cached by 
+        /// private (client) caches. (public or private directive)
+        /// 
+        /// Defaults to public.
+        /// </summary>
+        public CacheLocation CacheLocation { get; set; } = CacheLocation.Public;
+
+        /// <summary>
+        /// When true, the no-store directive is included in the Cache-Control header.  
+        /// When this directive is included, a cache must not store any part of the message,
+        /// mostly for confidentiality reasonse.  
+        /// 
+        /// Defaults to false.
+        /// </summary>
+        public bool AddNoStoreDirective { get; set; } = false;
+
+        /// <summary>
+        /// When true, the no-transform directive is included in the Cache-Control header.
+        /// When this directive is included, a cache must not convert the media type of the 
+        /// response body. 
+        /// 
+        /// Defaults to false.
+        /// </summary>
+        public bool AddNoTransformDirective { get; set; } = false;
+
+    }
+}
---
+++ src/Marvin.Cache.Headers/ValidationModelOptions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marvin.Cache.Headers
+{
+    /// <summary>
+    /// Options that have to do with the validation model, mainly related to ETag generation, Last-Modified on the response,
+    /// but also to the Cache-Control header (as that is used for both expiration & validation requirements)
+    /// </summary>
+    public class ValidationModelOptions
+    {
+        /// <summary>
+        /// A case-insensitive list of headers from the request to take into account as differentiator
+        /// between requests (eg: for generating ETags)
+        /// 
+        /// Defaults to Accept, Accept-Language.  * indicates all headers will be taken into account.
+        /// </summary>
+        public IEnumerable<string> Vary { get; set; } = new List<string>() { "Accept", "Accept-Language" };
+
+        /// <summary>
+        /// When true, the no-cache directive is added to the Cache-Control header.
+        /// This indicates to a cache that the response should not be used for subsequent requests 
+        /// without successful revalidation with the origin server.
+        /// 
+        /// Defaults to false.
+        /// </summary>
+        public bool AddNoCache { get; set; } = false;
+
+        /// <summary>
+        /// When true, the must-revalidate directive is added to the Cache-Control header.
+        /// This tells a cache that if a response becomes stale, ie: it's expired, revalidation has to happen. 
+        /// By adding this directive, we can force revalidation by the cache even if the client 
+        /// has decided that stale responses are for a specified amount of time (which a client can 
+        /// do by setting the max-stale directive).
+        /// 
+        /// Defaults to false.
+        /// </summary>
+        public bool AddMustRevalidate { get; set; } = false;
+
+        /// <summary>
+        /// When true, the proxy-revalidate directive is added to the Cache-Control header.
+        /// Exactly the same as must-revalidate, but only only for shared caches.  
+        /// So: this tells a shared cache that if a response becomes stale, ie: it's expired, revalidation has to happen. 
+        /// By adding this directive, we can force revalidation by the cache even if the client 
+        /// has decided that stale responses are for a specified amount of time (which a client can 
+        /// do by setting the max-stale directive).
+        /// 
+        /// Defaults to false.
+        /// </summary>
+        public bool AddProxyRevalidate { get; set; } = false;
+
+    }
+}
