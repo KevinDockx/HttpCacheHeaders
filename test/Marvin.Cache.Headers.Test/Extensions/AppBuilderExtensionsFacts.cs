41a6fba8f04892e4b0b7856c285e1b80474547a5
Ken Bonny <bonny.ken@gmail.com>
2017-01-09 21:48:07 +0100

    Add first draft of AppBuilderExtension tests.

---
+++ test/Marvin.Cache.Headers.Test/Extensions/AppBuilderExtensionsFacts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Moq;
+using Xunit;
+
+namespace Marvin.Cache.Headers.Test.Extensions
+{
+    public class AppBuilderExtensionsFacts
+    {
+        [Fact(Skip = "The Verify throws an exception because UseMiddleware is an extension function as well and can't be mocked, need to find ")]
+        public void Correctly_register_HttpCacheHeadersMiddleware()
+        {
+            var appBuilderMock = new Mock<IApplicationBuilder>();
+            appBuilderMock.Object.UseHttpCacheHeaders();
+
+            appBuilderMock.Verify(x => x.UseMiddleware<HttpCacheHeadersMiddleware>(), "Application builder isn't registering the middleware.");
+        }
+
+        [Fact]
+        public void When_no_ApplicationBuilder_expect_ArgumentNullException()
+        {
+            IApplicationBuilder appBuilder = null;
+
+            Assert.Throws<ArgumentNullException>(() => appBuilder.UseHttpCacheHeaders());
+
+        }
+    }
+}
