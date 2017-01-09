077043ee340397893c1c30380f0e508a61b5070c
Maarten Balliauw <maarten@maartenballiauw.be>
2017-01-09 12:11:59 +0100

    #17 - Add boiler-plate unit tests

---
+++ test/Marvin.Cache.Headers.Test/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Marvin.Cache.Headers.Stores;
+using Xunit;
+
+namespace Marvin.Cache.Headers.Test
+{
+    public class InMemoryValidationValueStoreFacts
+    {
+        [Fact]
+        public async Task GetAsync_Returns_Stored_ValidationValue()
+        {
+            // arrange
+            var referenceTime = DateTimeOffset.UtcNow;
+
+            var target = new InMemoryValidationValueStore();
+            await target.SetAsync("test-key", new ValidationValue(new ETag(ETagType.Strong, "test"), referenceTime));
+
+            // act
+            var result = await target.GetAsync("test-key");
+
+            // assert
+            Assert.NotNull(result);
+            Assert.Equal(result.ETag.ETagType, ETagType.Strong);
+            Assert.Equal(result.ETag.Value, "test");
+            Assert.Equal(result.LastModified, referenceTime);
+        }
+
+        [Fact]
+        public async Task GetAsync_DoesNotReturn_Unknown_ValidationValue()
+        {
+            // arrange
+            var referenceTime = DateTimeOffset.UtcNow;
+
+            var target = new InMemoryValidationValueStore();
+            await target.SetAsync("test-key", new ValidationValue(new ETag(ETagType.Strong, "test"), referenceTime));
+
+            // act
+            var result = await target.GetAsync("test-nonexisting-key");
+
+            // assert
+            Assert.Null(result);
+        }
+    }
+}
