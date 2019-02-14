109df98be528cc17b17cf0a04a218bd347a8f8a3
Kevin Dockx <kevin.dockx@gmail.com>
2019-02-14 10:36:32 +0100

    Add ILastModifiedInjector interface

---
+++ src/Marvin.Cache.Headers/Interfaces/ILastModifiedInjector.cs
@@ -0,0 +1,21 @@
+﻿using Marvin.Cache.Headers.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marvin.Cache.Headers.Interfaces
+{
+
+    /// <summary>
+    /// Contract for a LastModifiedInjector, which can be used to inject custom last modified dates for resources
+    /// of which you know when they were last modified (eg: a DB timestamp, custom logic, ...)
+    /// </summary>
+    public interface ILastModifiedInjector
+    {
+        Task<DateTimeOffset> CalculateLastModified(
+            StoreKey storeKey,
+            ResourceContext context);
+    }
+     
+}
