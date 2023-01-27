5ded872a0023d38b382f886cfa8d8829fa085a3b
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2023-01-27 22:04:28 +0100

    feat: add middleware option to ignore caching on specific status codes

--- src/Marvin.Cache.Headers/Domain/HttpCacheHeadersMiddlewareOptions.cs
+++ src/Marvin.Cache.Headers/Domain/HttpCacheHeadersMiddlewareOptions.cs
@@ -1,16 +1,28 @@
 ﻿// Any comments, input: @KevinDockx
 // Any issues, requests: https://github.com/KevinDockx/HttpCacheHeaders
 
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Marvin.Cache.Headers
 {
     /// <summary>
-    /// Options that have to do with the HttpCacheHeadersMiddleware, mainly to do with ignoring caching globally.
+    /// Options that have to do with the HttpCacheHeadersMiddleware, mainly with ignoring caching globally.
     /// </summary>
     public class HttpCacheHeadersMiddlewareOptions
     {
         /// <summary>
-        /// Ignore caching on a global level, defaults to false.
+        /// Ignore caching on a global level
+        ///
+        /// Defaults to false.
         /// </summary>
         public bool IgnoreCaching { get; set; } = false;
+        
+        /// <summary>
+        /// Ignore caching on responses with specific status codes.
+        ///
+        /// Defaults to none.
+        /// </summary>
+        public IEnumerable<int> IgnoredStatusCodes { get; set; } = Enumerable.Empty<int>();
     }
-    }
+}
