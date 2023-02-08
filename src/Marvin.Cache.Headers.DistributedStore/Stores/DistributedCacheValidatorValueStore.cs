2c551157c976b913a5d224b5eaef7d507203357f
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-02-08 10:16:02 +0000

    Make the DistributedCacheValidatorValueStore implement the IValidatorValueStore interface.

--- src/Marvin.Cache.Headers.DistributedStore/Stores/DistributedCacheValidatorValueStore.cs
+++ src/Marvin.Cache.Headers.DistributedStore/Stores/DistributedCacheValidatorValueStore.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Marvin.Cache.Headers.DistributedStore.Interfaces;
+using Marvin.Cache.Headers.Interfaces;
 using Microsoft.Extensions.Caching.Distributed;
 
 namespace Marvin.Cache.Headers.DistributedStore.Test.Stores
 {
-    public class DistributedCacheValidatorValueStore
+    public class DistributedCacheValidatorValueStore : IValidatorValueStore
     {
         private readonly IDistributedCache _distributedCache;
         private readonly IRetrieveDistributedCacheKeys _distributedCacheKeyRetriever;
@@ -13,6 +16,26 @@ namespace Marvin.Cache.Headers.DistributedStore.Test.Stores
         {
             _distributedCache = distributedCache ?? throw new ArgumentNullException(nameof(distributedCache));
             _distributedCacheKeyRetriever = distributedCacheKeyRetriever ?? throw new ArgumentNullException(nameof(distributedCacheKeyRetriever));
+        }
+
+        public async Task<ValidatorValue> GetAsync(StoreKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SetAsync(StoreKey key, ValidatorValue validatorValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RemoveAsync(StoreKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<StoreKey>> FindStoreKeysByKeyPartAsync(string valueToMatch, bool ignoreCase)
+        {
+            throw new NotImplementedException();
         }
     }
 }
