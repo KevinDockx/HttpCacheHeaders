b09b8f0b1e3724cdbbe76eba9738da6466049db9
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-02-08 07:57:36 +0000

    Add a skeleton class representing a distributed cache store.

---
+++ src/Marvin.Cache.Headers.DistributedStore/Stores/DistributedCacheValidatorValueStore.cs
@@ -0,0 +1,10 @@
+﻿// Any comments, input: @KevinDockx
+// Any issues, requests: https://github.com/KevinDockx/HttpCacheHeaders
+
+namespace Marvin.Cache.Headers.DistributedStore.Stores
+{
+    public class DistributedCacheValidatorValueStore
+    {
+        
+    }
+}
