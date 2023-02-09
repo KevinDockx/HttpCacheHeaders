0bbe0ca79f368ba1fb56d1fef093080b08f66d50
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-02-09 17:36:53 +0000

    Add a failing test that ensures an ArgumentNullException is thrown when a null ValueToMatch parameter is passed in to the FindStoreKeysByKeyPartAsync method of the DistributedCacheValidatorValueStore class.

--- src/Marvin.Cache.Headers.DistributedStore/Interfaces/IRetrieveDistributedCacheKeys.cs
+++ src/Marvin.Cache.Headers.DistributedStore/Interfaces/IRetrieveDistributedCacheKeys.cs
@@ -1,7 +1,10 @@
-﻿namespace Marvin.Cache.Headers.DistributedStore.Interfaces
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Marvin.Cache.Headers.DistributedStore.Interfaces
 {
     public interface IRetrieveDistributedCacheKeys
     {
-        
+        Task<IEnumerable<string>> FindStoreKeysByKeyPartAsync(string valueToMatch, bool ignoreCase);
     }
 }
