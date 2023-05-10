78b960a29d11dec8ce32e970f0d386ec1d79624d
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-05-10 10:55:22 +0100

    Add the ability to specify the redis database to be used along with whether to use replicas in the RedisDistributedCacheKeyRetrieverOptions class.

--- src/Marvin.Cache.Headers.DistributedStore.Redis/Options/RedisDistributedCacheKeyRetrieverOptions.cs
+++ src/Marvin.Cache.Headers.DistributedStore.Redis/Options/RedisDistributedCacheKeyRetrieverOptions.cs
@@ -2,5 +2,14 @@
 {
     public class RedisDistributedCacheKeyRetrieverOptions
     {
+        /// <summary>
+        /// The Redis database you wish keys to be retrieved from.
+        /// </summary>
+        public int Database { get; set; }
+        
+        /// <summary>
+        /// Indicates whether only replicas should be used when choosing the servers to iterate looking for keys in the selected database.
+        /// </summary>
+        public bool OnlyUseReplicas { get; set;  }
     }
 }
