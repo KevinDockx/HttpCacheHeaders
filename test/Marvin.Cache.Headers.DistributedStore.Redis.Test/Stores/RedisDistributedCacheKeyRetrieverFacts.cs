7a7efe3a2295776e1045365ddddc2f980a20e8da
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-05-10 10:10:46 +0100

    Add a failing test that ensures an ArgumentNullException is thrown if a null redisDistributedCacheKeyRetrieverOptions parameter is passed to the constructor of the RedisDistributedCacheKeyRetriever class.

--- test/Marvin.Cache.Headers.DistributedStore.Redis.Test/Stores/RedisDistributedCacheKeyRetrieverFacts.cs
+++ test/Marvin.Cache.Headers.DistributedStore.Redis.Test/Stores/RedisDistributedCacheKeyRetrieverFacts.cs
@@ -18,4 +18,13 @@ public class RedisDistributedCacheKeyRetrieverFacts
         var exception = Record.Exception(() => new RedisDistributedCacheKeyRetriever(connectionMultiplexer, redisDistributedCacheKeyRetrieverOptions.Object));
         Assert.IsType<ArgumentNullException>(exception);
     }
+
+    [Fact]
+    public void Throws_ArgumentNullException_When_A_NullRedis_Distributed_Cache_Key_Retriever_Options_Is_Passed_in()
+    {
+        var connectionMultiplexer = new Mock<IConnectionMultiplexer>();
+        IOptions<RedisDistributedCacheKeyRetrieverOptions> redisDistributedCacheKeyRetrieverOptions = null;
+        var exception = Record.Exception(() => new RedisDistributedCacheKeyRetriever(connectionMultiplexer.Object, redisDistributedCacheKeyRetrieverOptions));
+        Assert.IsType<ArgumentNullException>(exception);
+    }
 }
