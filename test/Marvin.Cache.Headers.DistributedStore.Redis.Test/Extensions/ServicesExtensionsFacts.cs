b218c82b9aea59a178a0210eed91822fab7ec6d4
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-05-20 11:19:01 +0100

    Add a failing test that ensures An ArgumentNullException is thrown when the redisDistributedCacheKeyRetrieverOptionsAction  passed to the AddRedisKeyRetriever method of the ServicesExtensions class is null.

--- test/Marvin.Cache.Headers.DistributedStore.Redis.Test/Extensions/ServicesExtensionsFacts.cs
+++ test/Marvin.Cache.Headers.DistributedStore.Redis.Test/Extensions/ServicesExtensionsFacts.cs
@@ -3,6 +3,7 @@ using Marvin.Cache.Headers.DistributedStore.Redis.Options;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using System;
+using Moq;
 using Xunit;
 
 namespace Marvin.Cache.Headers.DistributedStore.Redis.Test.Extensions;
@@ -15,5 +16,13 @@ public class ServicesExtensionsFacts
         IServiceCollection? services = null;
         Action<IOptions<RedisDistributedCacheKeyRetrieverOptions>> redisDistributedCacheKeyRetrieverOptionsAction =o =>{};
         Assert.Throws<ArgumentNullException>(() => services.AddRedisKeyRetriever(redisDistributedCacheKeyRetrieverOptionsAction));
+    }
+    
+    [Fact]
+    public void AddRedisKeyRetriever_Throws_An_Argument_Null_Exception_When_The_RedisDistributedCacheKeyRetrieverOptionsAction_Parameter_Passed_In_Is_Null2()
+    {
+        var services = new Mock<IServiceCollection>();
+        Action<IOptions<RedisDistributedCacheKeyRetrieverOptions>>? redisDistributedCacheKeyRetrieverOptionsAction = null;
+        Assert.Throws<ArgumentNullException>(() => services.Object.AddRedisKeyRetriever(redisDistributedCacheKeyRetrieverOptionsAction));
     }
 }
