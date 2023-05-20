c9d5b3d819569d2ea18e7d4989d86f96a4a34d4b
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-05-20 13:09:09 +0100

    Add documentation.

--- src/Marvin.Cache.Headers.DistributedStore.Redis/Extensions/ServicesExtensions.cs
+++ src/Marvin.Cache.Headers.DistributedStore.Redis/Extensions/ServicesExtensions.cs
@@ -7,8 +7,16 @@ using Microsoft.Extensions.Options;
 
 namespace Marvin.Cache.Headers.DistributedStore.Redis.Extensions
 {
+    /// <summary>
+    /// Extension methods for the HttpCache middleware (on IServiceCollection)
+    /// </summary>
     public static class ServicesExtensions
     {
+        /// <summary>
+        /// Add redis key retriever services to the service collection.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+        /// <param name="redisDistributedCacheKeyRetrieverOptionsAction">Action to provide custom <see cref="RedisDistributedCacheKeyRetrieverOptions" /></param>
         public static IServiceCollection AddRedisKeyRetriever(this IServiceCollection services, Action<IOptions<RedisDistributedCacheKeyRetrieverOptions>> redisDistributedCacheKeyRetrieverOptionsAction)
         {
             if (services == null)
